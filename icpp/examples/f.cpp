8de7edc4db11ae3b32a25b0abd9e6dbbd15f2afa
Krozark <maxime.barbier1991@gmail.com>
2014-07-09 01:22:19 +0200

    add func files

--- icpp/examples/f.cpp
+++ icpp/examples/f.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 extern "C"{
-    int print(int i){
+    int test_print(int i){
         std::cout<<i<<std::endl;
         return 42;
     }
---
+++ icpp/examples/f2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+extern "C"{
+    void test_print2(){
+        std::cout<<"test_print2"<<std::endl;
+    }
+}
